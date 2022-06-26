1bb24d6403c933d08aef064a1e711b59bbd08cdf
Luka <luka.jengic@gmail.com>
2022-06-27 00:38:00 +0200

    a opet, spavanje, sta drugo ...

--- images/BWP_and_RTO_plot.py
+++ images/BWP_and_RTO_plot.py
@@ -8,8 +8,18 @@ plt.rcParams["figure.autolayout"] = True
 
 headers = ['Utilization', 'BWP','RTO']
 
-df = pd.read_csv('BWP.csv', names=headers)
+df = pd.read_csv('BWP_and_RTO.csv', names=headers)
 
 df.set_index('Utilization').plot()
+
+plt.xlabel('Faktor opterećenja')
+plt.ylabel('Kvaliteta usluge')
+
+plt.xlim(0.85,1.55)
+plt.ylim(0,1.2)
+
+f = plt.figure()
+f.set_figwidth(4)
+f.set_figheight(4)
 
 plt.show()
---
+++ images/EDF1_plot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+headers = ['Utilization', 'EDF',]
+
+df = pd.read_csv('EDF1.csv', names=headers)
+
+df.set_index('Utilization').plot()
+
+plt.xlabel('Faktor opterećenja')
+plt.ylabel('Kvaliteta usluge')
+
+plt.xlim(0.85,1.55)
+plt.ylim(0.5,1.1)
+
+f = plt.figure()
+f.set_figwidth(4)
+f.set_figheight(4)
+
+plt.show()
