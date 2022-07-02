e19eddefeae4f23fd9bcaaf6e3aaacda9a5ed314
Luka <luka.jengic@gmail.com>
2022-07-02 10:34:59 +0200

    poravnat tekst

--- images/EDF2_plot.py
+++ images/EDF2_plot.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-headers = ['Utilization', 'BWP','RTO']
+headers = ['Utilization', 'EDF',]
 
-df = pd.read_csv('BWP_and_RTO.csv', names=headers)
+df = pd.read_csv('EDF2.csv', names=headers)
 
 df.set_index('Utilization').plot()
 
@@ -16,7 +16,7 @@ plt.xlabel('Faktor opterećenja')
 plt.ylabel('Kvaliteta usluge')
 
 plt.xlim(0.85,1.55)
-plt.ylim(0,1.2)
+plt.ylim(0.8,1.1)
 
 f = plt.figure()
 f.set_figwidth(4)
